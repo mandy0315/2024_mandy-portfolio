a7b0f6d8b66155b7fce3f80acdbd84eb18945872
Mandy Tsai <mandy3152618@gmail.com>
2024-01-16 14:13:02 +0800

    Updates

---
+++ static/index.yzV-L3pN.js
@@ -0,0 +1,1 @@
+import{_ as e,v as t,t as s}from"./entry.LoJ-9GSR.js";const c={},o={class:"test-lg test"};function n(r,_){return s(),t("div",o,"Hello")}const l=e(c,[["render",n]]);export{l as default};
