f4957aa81e23864fd61c27421d24af55b608ddcf
Mandy Tsai <mandy3152618@gmail.com>
2024-01-16 11:40:25 +0800

    Updates

---
+++ asset/skills.SVrcB7UO.js
@@ -0,0 +1,1 @@
+import{d as e,t as s,v as t}from"./entry.9Jb70NLG.js";const o={class:"test-lg test"},_=e({__name:"skills",setup(n){return(a,c)=>(s(),t("div",o,"skills"))}});export{_ as default};
---
+++ asset/works.RYc6NeHC.js
@@ -0,0 +1,1 @@
+import{d as e,t,v as s}from"./entry.9Jb70NLG.js";const o={class:"test-lg test"},_=e({__name:"works",setup(n){return(r,a)=>(t(),s("div",o,"works"))}});export{_ as default};
