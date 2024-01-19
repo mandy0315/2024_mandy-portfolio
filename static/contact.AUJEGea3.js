3b25acf18ce7a14526a8936eb872bd0d24cb6787
Mandy Tsai <mandy3152618@gmail.com>
2024-01-19 09:38:45 +0800

    Updates

---
+++ static/contact.AUJEGea3.js
@@ -0,0 +1,1 @@
+import{f as t,o as e,c}from"./entry.k3rnFaNW.js";const o={class:"test-lg test"},r=t({__name:"contact",setup(s){return(n,a)=>(e(),c("div",o,"contact"))}});export{r as default};
---
+++ static/works.N45EqGOX.js
@@ -0,0 +1,1 @@
+import{f as e,o as t,c as s}from"./entry.k3rnFaNW.js";const o={class:"test-lg test"},_=e({__name:"works",setup(c){return(n,r)=>(t(),s("div",o,"works"))}});export{_ as default};
